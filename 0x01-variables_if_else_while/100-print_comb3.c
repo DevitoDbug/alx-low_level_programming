7972eaeb36ca45deecc1b69611a353beeffaef20
David Ochieng <davidochiengy@gmail.com>
2022-09-10 11:13:20 -0700

    C basics

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,9 +13,9 @@ int main(void)
 	int j;
 	int i;
 
-	for (i = 49; i <= 57; i++)
+	for (i = 48; i <= 57; i++)
 	{
-		for (j = i+1; j <= 57; j++)
+		for (j = i + 1; j <= 57; j++)
 		{
 			putchar(i);
 			putchar(j);
