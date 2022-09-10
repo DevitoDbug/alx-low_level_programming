03b6c54881c5cf532c340008f4ab3d72735380c4
David Ochieng <davidochiengy@gmail.com>
2022-09-10 04:13:32 -0700

    C basics

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+		putchar(i);
+	 for (i = 65; i <= 70; i++)                                                                          
+		putchar(i); 
+	putchar('\n');
+	return (0);
+}
