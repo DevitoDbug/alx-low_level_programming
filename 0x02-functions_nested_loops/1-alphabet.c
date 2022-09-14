08b8c3cfc29c07c783c5ab43804637fc85ecc64b
David Ochieng <davidochiengy@gmail.com>
2022-09-14 12:01:28 -0700

    C Basics 2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -11,7 +11,7 @@ void print_alphabet(void)
 	int i;
 	char letters[26] = "abcdefghijklmnopqrstuvwxyz";
 
-	for (i = 0; i <=25; i++)
+	for (i = 0; i <= 25; i++)
 		_putchar(letters[i]);
 	_putchar('\n');
 }
