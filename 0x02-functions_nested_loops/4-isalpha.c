d808357f53279f0d064f7c6a45097b10d690e361
David Ochieng <davidochiengy@gmail.com>
2022-09-14 13:21:22 -0700

    C Basics 2

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,13 +3,13 @@
 #include "main.h"
 
 /**
- *_islower - shows if the letter entered is lower case
+ * _isalpha - shows if the letter entered is lower case
  *@c: letter being tested
  * Return: Always 0.
  */
-int _islower(int c)
+int _isalpha(int c)
 {
-	if (islower(c) > 0)
+	if (isalpha(c) > 0)
 		return (1);
 	else
 		return (0);
