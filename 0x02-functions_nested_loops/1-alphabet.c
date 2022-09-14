d75ca2d45c18b784e289209e8cd4dbd52ef7e883
David Ochieng <davidochiengy@gmail.com>
2022-09-14 11:47:24 -0700

    C Basics 2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,16 @@
-#include "main.h"
+#include <unistd.h>
 
 /**
  *main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
-
-int main(void)
+void print_alphabet (void)
 {
-	print_alphabet();
-	return (0);
+	int i;
+	char letters[26] = "abcdefghijklmnopqrstuvwxyz";
+
+	for (i = 0; i <=26; i++)
+		_putchar(letters[i]);
+	_putchar('\n');
 }
--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,18 +1,13 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
- * _putchar - writes the characters to stdout
+ * main - check the code for Holberton School students.
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: Always 0.
  */
 
-void print_alphabet(void)
+int main(void)
 {
-	int i;
-	char letters[26] = "abcdefghijklmnopqrstuvwxyz";
-	
-	 for (i = 0; i <=26; i++)
-		 _putchar(letters[i]);
-	 _putchar('\n');
+	print_alphabet();
+	return (0);
 }
