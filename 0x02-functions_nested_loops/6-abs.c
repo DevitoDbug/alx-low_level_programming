658a27c300bd97a3ece84e4943224d313ea54d4d
David Ochieng <davidochiengy@gmail.com>
2022-09-14 13:52:01 -0700

    C Basics 2

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ *_abs - gives the absolute value of c
+ *@c: number  being manupilated
+ * Return: Always 0.
+ */
+int _abs(int c)
+{
+	return (abs(c));
+}
