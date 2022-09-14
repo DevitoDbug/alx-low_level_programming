326f3ea3d5473d67c388b28dd1c8010ea8f65458
David Ochieng <davidochiengy@gmail.com>
2022-09-14 12:09:55 -0700

    C Basics 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ *print_alphabet - prints the letters of the alphabet
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	int i, j;
+	char letters[26] = "abcdefghijklmnopqrstuvwxyz";
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 0; i <= 25; i++)
+			_putchar(letters[i]);
+		_putchar('\n');
+	}
+}
