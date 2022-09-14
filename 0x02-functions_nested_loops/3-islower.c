79749da647a897ab2884f7caadfdb20cdd1f437d
David Ochieng <davidochiengy@gmail.com>
2022-09-14 12:42:00 -0700

    C Basics 2

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ *int _islower - shows if the letter entered is lower case 
+ *
+ * Return: Always 0.
+ */
+int _islower(int c)
+{
+	if (islower(c) > 0)
+		returns 1;
+	else
+		returns 0;
+}
