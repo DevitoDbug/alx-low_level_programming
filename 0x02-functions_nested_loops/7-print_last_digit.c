d9297b14819f300a889e07a20977609908b564a5
David Ochieng <davidochiengy@gmail.com>
2022-09-14 14:07:20 -0700

    C Basics 2

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ *print_last_digit - prints the last digit 
+ *@c: number  being manupilated
+ * Return: Always 0.
+ */
+int print_last_digit(int c)
+{
+	int c;
+
+	x = (c % 10);
+	return (x);
+}
