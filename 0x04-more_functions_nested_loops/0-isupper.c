292de2c15275de06b2df24faf781d7d596d18b3a
David Ochieng <davidochiengy@gmail.com>
2022-09-16 19:17:11 -0700

    C advanced 1

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - evalue if letter is uppercase .
+ *@c: print int
+ * Return: Always 0.
+ */
+
+int _isupper(int c)
+{
+	if (isupper(c) == 1)
+		return (1);
+	else
+		return (0);
+}
