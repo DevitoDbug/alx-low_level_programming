cd5c07ca6c5779680194f264da7392c9c791f3a5
David Ochieng <davidochiengy@gmail.com>
2022-09-16 20:53:12 -0700

    C advanced 1

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *print_square - prints a squar
+ *@size: print int
+ * Return: Always 0.
+ */
+
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	if (n > 0)
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				_putchar (35);
+			}
+			_putchar ('\n');
+		}
+	else
+			_putchar ('\n');
+}
